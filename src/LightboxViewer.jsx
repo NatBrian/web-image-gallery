7b30ad659011cd6d9a4e91be528369b59140de5c
Nathanael Brian <22826533+NatBrian@users.noreply.github.com>
2025-10-27 15:57:20 +0700

    add zoom and download to lightbox

--- src/LightboxViewer.jsx
+++ src/LightboxViewer.jsx
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Lightbox from 'yet-another-react-lightbox';
 import 'yet-another-react-lightbox/styles.css';
 
+// Plugins
+import Download from 'yet-another-react-lightbox/plugins/download';
+import Zoom from 'yet-another-react-lightbox/plugins/zoom';
+
 const LightboxViewer = ({ images, currentImage, setCurrentImage, closeLightbox }) => {
-  // Convert image URLs to the format expected by yet-another-react-lightbox
-  const slides = images.map((imgUrl, index) => ({
-    src: imgUrl,
-    alt: `Image ${index + 1}`,
-    width: 1920,
-    height: 1080
-  }));
+  const [slides, setSlides] = useState([]);
+
+  useEffect(() => {
+    // Preload and get actual dimensions for better zoom quality
+    const loadSlides = async () => {
+      const loadedSlides = await Promise.all(
+        images.map(async (imgUrl, index) => {
+          const img = new Image();
+          img.src = imgUrl;
+          await img.decode();
+          return {
+            src: imgUrl,
+            alt: `Image ${index + 1}`,
+            width: img.naturalWidth,
+            height: img.naturalHeight,
+          };
+        })
+      );
+      setSlides(loadedSlides);
+    };
+
+    loadSlides();
+  }, [images]);
 
   return (
     <Lightbox
@@ -18,11 +38,26 @@ const LightboxViewer = ({ images, currentImage, setCurrentImage, closeLightbox }
       slides={slides}
       index={currentImage}
       on={{
-        view: ({ index }) => setCurrentImage(index),
+        view: ({ index }) => {
+          if (typeof setCurrentImage === 'function') {
+            setCurrentImage(index);
+          }
+        },
       }}
-      animation={{ fade: 200 }}
-      controller={{ closeOnPullDown: true, closeOnBackdropClick: true }}
-      carousel={{ finite: false }}
+      plugins={[Download, Zoom]}
+      zoom={{
+        maxZoomPixelRatio: 3, // how much you can zoom in
+        zoomInMultiplier: 1.5,
+        doubleTapDelay: 300,
+      }}
+      download={{
+        download: true,
+        title: 'Download image',
+      }}
+      controller={{
+        closeOnBackdropClick: true,
+        closeOnPullDown: true,
+      }}
     />
   );
 };
